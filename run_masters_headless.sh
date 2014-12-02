8f4f4e227f751bd262d86c9307bf71ce4ebc8549
Michele Silva <michele.silva@gmail.com>
2014-12-02 10:39:55 -0200

    Allow running masters headless script from a different directory

--- run_masters_headless.sh
+++ run_masters_headless.sh
@@ -1,4 +1,6 @@
+cd `dirname $0`
 if [ ! -f MastersWrapper.class ]; then
-    sh ./build_masters_wrapper.sh
+    sh build_masters_wrapper.sh
 fi
 java -Djava.library.path=./Netlogo-MASTERS-linux/lib -Dorg.nlogo.is3d=true -cp .:Netlogo-MASTERS-linux/NetLogo.jar MastersWrapper
+cd - > /dev/null
