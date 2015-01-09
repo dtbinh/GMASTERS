8b6f4652b029520a11d19a109355dcb8942e48f3
Michele Silva <michele.s@icanlocalize.com>
2015-01-09 13:31:16 -0200

    Improve build scripts name

--- run_masters_gateway.sh
+++ run_masters_gateway.sh
@@ -1,6 +1,6 @@
 cd `dirname $0`
 if [ ! -f masters/MastersServer.class ]; then
-    sh build_masters_wrapper.sh
+    sh build_masters_gateway
 fi
 java -Djava.library.path=./Netlogo-MASTERS-linux/lib -Dorg.nlogo.is3d=true -cp .:Netlogo-MASTERS-linux/NetLogo.jar:/usr/local/share/py4j/py4j0.8.2.1.jar masters.MastersServer
 cd - > /dev/null
