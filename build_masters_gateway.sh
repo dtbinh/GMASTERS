f4fd7f50083f033496e963e9284004fc101df7cf
Michele Silva <michele.s@icanlocalize.com>
2015-01-09 15:00:31 -0200

    Add py4j lib to project

--- build_masters_gateway.sh
+++ build_masters_gateway.sh
@@ -1,1 +1,1 @@
-javac -cp .:Netlogo-MASTERS-linux/NetLogo.jar:/usr/local/share/py4j/py4j0.8.2.1.jar masters/*.java
+javac -cp .:Netlogo-MASTERS-linux/NetLogo.jar:lib/py4j0.8.2.1.jar masters/*.java
--- run_masters_gateway.sh
+++ run_masters_gateway.sh
@@ -2,5 +2,5 @@ cd `dirname $0`
 if [ ! -f masters/MastersServer.class ]; then
     sh build_masters_gateway
 fi
-java -Djava.library.path=./Netlogo-MASTERS-linux/lib -Dorg.nlogo.is3d=true -cp .:Netlogo-MASTERS-linux/NetLogo.jar:/usr/local/share/py4j/py4j0.8.2.1.jar masters.MastersServer
+java -Djava.library.path=./Netlogo-MASTERS-linux/lib -Dorg.nlogo.is3d=true -cp .:Netlogo-MASTERS-linux/NetLogo.jar:lib/py4j0.8.2.1.jar masters.MastersServer
 cd - > /dev/null
